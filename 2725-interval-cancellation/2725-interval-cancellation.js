1de7a748154e0eb868faa6007485df61433478de
mohammedidhi0225 <mohammedidrissss3@gmail.com>
2024-05-15 11:54:31 +0530

    Time: 72 ms (22.45%), Space: 49.6 MB (41.73%) - LeetHub

---
+++ 2725-interval-cancellation/2725-interval-cancellation.js
@@ -0,0 +1,7 @@
+var cancellable = function(fn, args, t) {
+    fn(...args);
+    const timer = setInterval(() => fn(...args), t);
+
+    const cancelFn = () => clearInterval(timer);
+    return cancelFn;    
+};
