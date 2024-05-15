45d5348f4669642534b62b59b8f691d844c411bd
mohammedidhi0225 <mohammedidrissss3@gmail.com>
2024-05-15 11:59:46 +0530

    Time: 394 ms (24.04%), Space: 100.3 MB (11.33%) - LeetHub

---
+++ 2722-join-two-arrays-by-id/2722-join-two-arrays-by-id.js
@@ -0,0 +1,19 @@
+
+var join = function(arr1, arr2) {
+    const combinedArray = arr1.concat(arr2);
+  const merged = {};
+
+  combinedArray.forEach((obj) => {
+    const id = obj.id;
+    if (!merged[id]) {
+      merged[id] = { ...obj };
+    } else {
+      merged[id] = { ...merged[id], ...obj };
+    }
+  });
+
+  const joinedArray = Object.values(merged);
+  joinedArray.sort((a, b) => a.id - b.id);
+
+  return joinedArray;
+};
